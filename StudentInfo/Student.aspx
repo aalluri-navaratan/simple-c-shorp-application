d2c49ff0dac650038bc0ae1e0607fc0932de9aa2
Alluri Anil kumar <alluri.anil190@gmail.com>
2015-08-13 13:19:59 +0530

    added the create, update and delete operations

---
+++ StudentInfo/Student.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Student.aspx.cs" Inherits="Student" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 488px">
+    
+        <asp:Label ID="Label1" runat="server" 
+            style="z-index: 1; left: 293px; top: 143px; position: absolute; width: 17px" 
+            Text="Id"></asp:Label>
+        <asp:Label ID="Label2" runat="server" 
+            style="z-index: 1; left: 291px; top: 177px; position: absolute; width: 39px" 
+            Text="name"></asp:Label>
+        <asp:Label ID="Label3" runat="server" 
+            style="z-index: 1; left: 294px; top: 214px; position: absolute" Text="Mail"></asp:Label>
+        <asp:TextBox ID="txtId" runat="server" 
+            style="z-index: 1; left: 390px; top: 142px; position: absolute"></asp:TextBox>
+        <asp:TextBox ID="txtname" runat="server" 
+            style="z-index: 1; left: 392px; top: 177px; position: absolute"></asp:TextBox>
+        <asp:TextBox ID="txtmail" runat="server" 
+            style="z-index: 1; left: 392px; top: 215px; position: absolute"></asp:TextBox>
+        <asp:Button ID="AddButton" runat="server" onclick="AddStudent" 
+            style="z-index: 1; left: 301px; top: 258px; position: absolute" Text="Insert" />
+        <asp:Button ID="UpdateButton" runat="server" onclick="UpdateStudent" 
+            style="z-index: 1; left: 387px; top: 259px; position: absolute; right: 668px" 
+            Text="Update" />
+        <asp:Button ID="DeleteButton" runat="server" onclick="DeleteStudent" 
+            style="z-index: 1; left: 481px; top: 259px; position: absolute" Text="Delete" />
+        <asp:GridView ID="GridView1" runat="server" 
+            style="z-index: 1; left: 754px; top: 143px; position: absolute; height: 133px; width: 187px">
+        </asp:GridView>
+        <asp:Label ID="ButtonInfo" runat="server" 
+            style="z-index: 1; left: 281px; top: 341px; position: absolute" Text="Label" 
+            Visible="False"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ StudentInfo/Student.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public partial class Student : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Bind();
+    }
+    protected void AddStudent(object sender, EventArgs e)
+    {
+        SqlConnection cn = new SqlConnection("user id=sa;password=123;database=AnilDb;server=Aneel-PC");
+        cn.Open();
+        SqlCommand cmd = new SqlCommand("insert into Student values("+txtId.Text+",'"+txtname.Text+"','"+txtmail.Text+"')",cn);
+        int i=cmd.ExecuteNonQuery();
+        cn.Close();
+        ButtonInfo.Visible = true;
+        if (i > 0)
+        {
+            ButtonInfo.Text = "Ok, Inserted";
+        }
+        else
+        {
+            ButtonInfo.Text = "Not, Inserted";
+        }
+        Bind();
+    }
+    protected void UpdateStudent(object sender, EventArgs e)
+    {
+        SqlConnection cn = new SqlConnection("user id=sa;password=123;database=AnilDb;server=Aneel-PC");
+        cn.Open();
+        SqlCommand cmd = new SqlCommand("update Student set Mail='" + txtmail.Text + "' where StudentId=" + txtId.Text + "", cn);
+        int i = cmd.ExecuteNonQuery();
+        cn.Close();
+        ButtonInfo.Visible = true;
+        if (i > 0)
+        {
+            ButtonInfo.Text = "Ok, Updated";
+        }
+        else
+        {
+            ButtonInfo.Text = "Not, Updated";
+        }
+        Bind();
+    }
+    protected void DeleteStudent(object sender, EventArgs e)
+    {
+        SqlConnection cn = new SqlConnection("user id=sa;password=123;database=AnilDb;server=Aneel-PC");
+        cn.Open();
+        SqlCommand cmd = new SqlCommand("delete Student where StudentId="+txtId.Text+"", cn);
+        int i = cmd.ExecuteNonQuery();
+        cn.Close();
+        ButtonInfo.Visible = true;
+        if (i > 0)
+        {
+            ButtonInfo.Text = "Ok, Deleted";
+        }
+        else
+        {
+            ButtonInfo.Text = "Not, Deleted";
+        }
+        Bind();
+    }
+    public void Bind()
+    {
+        SqlConnection cn = new SqlConnection("user id=sa;password=123;database=AnilDb;server=Aneel-PC");
+        SqlDataAdapter da = new SqlDataAdapter("select * from Student order by StudentId desc", cn);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+    }
+}
